6fa9cd21f6c8bf75eba87ca8e022234a527b0b07
Jass Dhillon <126587354+imkaurageous@users.noreply.github.com>
2024-09-20 10:18:07 +0530

    Add files via upload

---
+++ Logger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeApplication
+{
+    public class Logger
+    {
+        private readonly string _logDirectory;
+
+        public Logger(string logDirectory)
+        {
+            _logDirectory = logDirectory;
+
+            // Ensure the directory exists
+            if (!Directory.Exists(_logDirectory))
+            {
+                Directory.CreateDirectory(_logDirectory);
+            }
+        }
+
+
+        public void LogMessage(string message)
+        {
+            // Generate file name based on current date
+            string fileName = $"log_{DateTime.Now:yyyyMMdd}.txt";
+            string filePath = Path.Combine(_logDirectory, fileName);
+
+            // Prepare log entry
+            string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: {message}";
+
+            // Append log entry to the file
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, append: true))
+                {
+                    writer.WriteLine(logEntry);
+                }
+            }
+            catch (IOException ex)
+            {
+                // Handle file IO exceptions if necessary
+                Console.WriteLine($"An error occurred while writing to the file: {ex.Message}");
+            }
+        }
+
+
+    }
+}
---
+++ SqlData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeApplication
+{
+    public class SqlData
+    {
+        public static DataSet GetFillDataSet(string connString, string procName, params SqlParameter[] paramters)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+
+                using (var sqlConnection = new SqlConnection(connString))
+                {
+                    using (var command = sqlConnection.CreateCommand())
+                    {
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        command.CommandText = procName;
+                        if (paramters != null)
+                        {
+                            command.Parameters.AddRange(paramters);
+                        }
+                        //sqlConnection.Open();
+                        using (var da = new SqlDataAdapter(command))
+                        {
+                            // command.CommandType = CommandType.StoredProcedure;
+                            da.Fill(ds);
+                        }
+                    }
+                }
+                return ds;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public int ExecuteNonQuery(SqlConnection connection, SqlTransaction transaction, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+            using (SqlCommand cmdExecute = new SqlCommand())
+            {
+                PrepareCommand(cmdExecute, connection, transaction, commandType, commandText, commandParameters);
+                cmdExecute.CommandTimeout = 0;
+                int returnVal = cmdExecute.ExecuteNonQuery();
+                return returnVal;
+            }
+        }
+        private void PrepareCommand(SqlCommand command, SqlConnection connection, SqlTransaction transaction, CommandType commandType, string commandText, SqlParameter[] commandParameters)
+        {
+            command.Connection = connection;
+            command.CommandText = commandText;
+            command.Transaction = transaction;
+            command.CommandType = commandType;
+            if (commandParameters != null)
+            {
+                AttachParameters(command, commandParameters);
+            }
+            return;
+        }
+
+        private void AttachParameters(SqlCommand command, SqlParameter[] commandParameters)
+        {
+            foreach (SqlParameter param in commandParameters)
+            {
+                if (param.Value == null)
+                {
+                    param.Value = DBNull.Value;
+                }
+                else if (param.Value.ToString() == "1/1/1999 12:00:00 AM")
+                {
+                    param.Value = DBNull.Value;
+                }
+                else if (param.Value.ToString() == DateTime.MinValue.ToString())
+                {
+                    param.Value = DBNull.Value;
+                }
+                else if (param.Value.ToString() == "01/01/1999 00:00:00")
+                {
+                    param.Value = DBNull.Value;
+                }
+                command.Parameters.Add(param);
+            }
+        }
+    }
+}
